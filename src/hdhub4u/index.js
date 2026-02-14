edb3e83ae6fee165856f2a3abda960e2f1dbc77f
leokimp <leokimpese@gmail.com>
2026-02-14 17:41:22 +0530

    changes

--- src/hdhub4u/index.js
+++ src/hdhub4u/index.js
@@ -515,7 +515,7 @@ async function getStreams(tmdbId, mediaType, season, episode) {
                 name: res.quality,
                 title: `${displayTitle}${year ? ` (${year})` : ''} ${mediaType === 'tv' ? `S${season}E${episode}` : ''}`,
                 url: res.url,
-                size: res.size,
+                size: formatBytes(res.size),
                 headers: HEADERS
             });
         });
@@ -524,12 +524,7 @@ async function getStreams(tmdbId, mediaType, season, episode) {
             const qOrder = { '2160p': 10, '4k': 10, '1080p': 8 };
             const aOrder = qOrder[a.name.toLowerCase()] || 0;
             const bOrder = qOrder[b.name.toLowerCase()] || 0;
-            
-            if (bOrder !== aOrder){
-                return bOrder - aOrder;
-            }
-            
-            return b.size - a.size;
+            return bOrder - aOrder;
         });
 
 
